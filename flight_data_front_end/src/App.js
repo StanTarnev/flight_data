2cb61c97c5f518d0f337cc8008b91c612226386d
Stan Tarnev <stan.tarnev@gmail.com>
2023-09-11 17:16:17 +0100

    loaded json in React state

--- flight_data_front_end/src/App.js
+++ flight_data_front_end/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useEffect, useState } from 'react'
 
-function App() {
+export default function App() {
+  const [flights, setFlights] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:9000/flights')
+    .then(response => response.json())
+    .then(data => setFlights(data.flight))
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>From App Component</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+
