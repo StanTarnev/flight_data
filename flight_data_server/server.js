94566e2160879e282d19fdf262f462d54740b8de
Stan Tarnev <stan.tarnev@gmail.com>
2023-09-11 13:10:54 +0100

    added front-end app

---
+++ flight_data_server/server.js
@@ -0,0 +1,28 @@
+import { readFileSync } from 'fs'
+import { parseString } from 'xml2js'
+import express from 'express'
+import cors from 'cors'
+
+let flightDataJSON = null
+const XMLFlightData = readFileSync('flighdata_A.xml')
+parseString(XMLFlightData, {
+        mergeAttrs: true, 
+        explicitRoot: false,
+        explicitArray: false
+    }, (err, result) => {
+    if (err) {
+        throw err
+    }
+    flightDataJSON = result
+})
+
+const app = express()
+app.use(cors())
+
+app.get('/flights', (req, res) => {
+    res.json(flightDataJSON)
+})
+
+app.listen(9000,  function () {
+    console.log(`App running on port ${this.address().port}`)
+})
