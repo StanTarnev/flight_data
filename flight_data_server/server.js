857046bbf30863dce86fc25706d6c5ffd7f6f120
Stan Tarnev <stan.tarnev@gmail.com>
2023-09-11 01:55:07 +0100

    api served, initial version

--- flight_data_server/server.js
+++ flight_data_server/server.js
@@ -1,14 +1,24 @@
 import { readFileSync } from 'fs'
 import { parseString } from 'xml2js'
+import express from 'express'
+import cors from 'cors'
 
+let flightDataJSON = null
 const XMLFlightData = readFileSync('flighdata_A.xml')
-
-let flighDataJSON = null
-
-parseString(XMLFlightData, { mergeAttrs: true }, (err, result) => {
+parseString(XMLFlightData, { mergeAttrs: false }, (err, result) => {
     if (err) {
         throw err
     }
+    flightDataJSON = result
+})
 
-    flighDataJSON = JSON.stringify(result)
+const app = express()
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.json(flightDataJSON)
+})
+
+app.listen(9000,  function () {
+    console.log(`App running on port ${this.address().port}`)
 })
