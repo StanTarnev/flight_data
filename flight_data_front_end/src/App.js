a94610299fbeabeae76928793810b5be1b247d99
Stan Tarnev <stan.tarnev@gmail.com>
2023-09-12 00:19:31 +0100

    fixed 'useEffect' and added some <h2>s
    update

--- flight_data_front_end/src/App.js
+++ flight_data_front_end/src/App.js
@@ -8,11 +8,26 @@ export default function App() {
     fetch('http://localhost:9000/flights')
     .then(response => response.json())
     .then(data => setFlights(data.flight))
-  })
+  }, [])
 
   return (
     <div className="App">
-      <h1>From App Component</h1>
+      <h1>Flight Data Stats</h1>
+      <div>
+        <h2>Number of Flight Departing in the Morning</h2>
+      </div>
+      <div>
+        <h2>Proportion of Flights to Sweden</h2>
+      </div>
+      <div>
+        <h2>Proportion of Flights to Sweden</h2>
+      </div>
+      <div>
+        <h2>10 Most Popular Destination Airports</h2>
+      </div>
+      <div>
+        <h2>Average Journey Time between London Heathrow and Dubai</h2>
+      </div>
     </div>
   )
 }
